dde92ffef957f8e20e1b8317c82fe658bf3491b8
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-25 19:26:51 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -3,6 +3,8 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
+git submodule add https://github.com/malkist01/patch
+apply patch -p1 < add.patch
 curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/fs/patch.sh" | bash -s main
 # Add KernelSU
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
