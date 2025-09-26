d19e35b1cb4cd61ce780aa227066ec9bddd93f94
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-26 12:34:57 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -10,7 +10,7 @@ git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 sus
 cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
 cp susfs4ksu/kernel_patches/fs/* ./fs
 cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
-aplly patch -p1 < 50_add_susfs_in_kernel-4.9.patch
+patch -p1 < 50_add_susfs_in_kernel-4.9.patch
 
 # Add SukiSU-Ultra
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
