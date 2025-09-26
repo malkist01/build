4fa2363848d5314d3774cd81544afdc4129acc35
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-26 16:45:44 +0700

    Update santoni.sh

--- scripts/santoni.sh
+++ scripts/santoni.sh
@@ -11,7 +11,7 @@ cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
 patch -p1 < 50_add_susfs_in_kernel-4.9.patch
 
 # Add SukiSU-Ultra
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-test
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/KernelSU/main/kernel/setup.sh" | bash -s susfs
 
 #add KSU Config
 echo "CONFIG_KPROBES=y" >> ./arch/arm64/configs/santoni_defconfig
