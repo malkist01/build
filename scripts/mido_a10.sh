057fe1504c04ddd9b36865e024b390afb43fc3d0
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-26 22:44:08 +0700

    Update mido_a10.sh

--- scripts/mido_a10.sh
+++ scripts/mido_a10.sh
@@ -20,15 +20,7 @@ echo "Adding CONFIG_KSU.."
 echo "CONFIG_KSU=y" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_KSU_TRACEPOINT_HOOK=y" >> ./arch/arm64/configs/mido_defconfig
 clang() {
-    echo "Cloning clang"
-    if [ ! -d "clang" ]; then
-      mkdir -p "clang"
-      curl -Lo WeebX-Clang-20.0.0git.tar.gz "https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-20.0.0git-release/WeebX-Clang-20.0.0git.tar.gz"
-      tar -zxf WeebX-Clang-20.0.0git.tar.gz -C "clang" --strip-components=1
-        KBUILD_COMPILER_STRING="WeebX-Clang"
-        PATH="${PWD}/clang/bin:${PATH}"
-    fi
-    sudo apt install -y ccache
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r547379.git clang
     echo "Done"
 }
 
