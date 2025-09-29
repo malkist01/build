0cbaa443fd2fa944d77bfffd7a30bf5cd30a0496
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-29 12:31:39 +0700

    Update j6plus.sh

--- scripts/j6plus.sh
+++ scripts/j6plus.sh
@@ -4,6 +4,7 @@ git clone $REPO -b $BRANCH kernel
 cd kernel
 git clone --depth=1 https://github.com/malkist01/patch
 git submodule add https://github.com/rifsxd/KernelSU-Next
+git submodule init && git submodule update
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 #add KSU Config
 echo "# CONFIG_KSU_MANUAL_HOOK=°y" >> ./arch/arm/configs/j6primelte_defconfig
