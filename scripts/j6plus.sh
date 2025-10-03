73d6290559fb36a14e49de195dcc2b88a8b3169f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-03 20:05:20 +0700

    Update j6plus.sh

--- scripts/j6plus.sh
+++ scripts/j6plus.sh
@@ -3,7 +3,8 @@ rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
 curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/add/patch.sh" | bash -s main
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/main/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/setup.sh" | bash -s main
+aplly patch -p1 < test.patch
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
