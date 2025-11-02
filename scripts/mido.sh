4613526e7a903d33d30dd2ae9150ee6878cb6c0f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-11-02 23:05:01 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -7,8 +7,19 @@ git clone --depth=1 https://github.com/malkist01/patch
 curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/add/patch.sh" | bash -s main
 
 # Add KernelSU
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
-#add KSU Config
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.1.1
+         cd KernelSU-Next
+         wget https://raw.githubusercontent.com/ThRE-Team/KSU/refs/heads/next-susfs/0001-add-susfs-v1.5.5.patch
+         patch -p1 < 0001-add-susfs-v1.5.5.patch
+         cd ..
+         
+         git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
+         cp susfs4ksu/kernel_patches/fs/* ./fs
+         cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
+         cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
+         patch -p1 < 50_add_susfs_in_kernel-4.9.patch
+
+         make mrproper
 echo "# CONFIG_KPM is not set" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_KALLSYMS=y" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_KALLSYMS_ALL=y" >> ./arch/arm64/configs/mido_defconfig
@@ -18,7 +29,7 @@ echo "CONFIG_LINUX_COMPILE_BY=malkist" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_LINUX_COMPILE_HOST=hp jadul" >> ./arch/arm64/configs/mido_defconfig
 echo "Adding CONFIG_KSU.."
 echo "CONFIG_KSU=y" >> ./arch/arm64/configs/mido_defconfig
-echo "CONFIG_KSU_TRACEPOINT_HOOK=y" >> ./arch/arm64/configs/mido_defconfig
+echo "CONFIG_KSUSFS=y" >> ./arch/arm64/configs/mido_defconfig
 clang() {
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
