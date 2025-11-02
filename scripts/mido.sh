70f1f13346b0b7b40bfce9442acdbc61e478dc3f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-11-02 23:35:37 +0700

    Update mido.sh

--- scripts/mido.sh
+++ scripts/mido.sh
@@ -3,19 +3,17 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
-         curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.1.1
-         cd KernelSU-Next
-         wget https://raw.githubusercontent.com/ThRE-Team/KSU/refs/heads/next-susfs/0001-add-susfs-v1.5.5.patch
-         patch -p1 < 0001-add-susfs-v1.5.5.patch
-         cd ..
-         
-         git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
-         cp susfs4ksu/kernel_patches/fs/* ./fs
-         cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
-         cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
-         patch -p1 < 50_add_susfs_in_kernel-4.9.patch
-
-         make mrproper
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.1.1
+cd KernelSU-Next
+wget https://raw.githubusercontent.com/ThRE-Team/KSU/refs/heads/next-susfs/0001-add-susfs-v1.5.5.patch
+patch -p1 < 0001-add-susfs-v1.5.5.patch
+cd ..
+git clone --depth=1 https://gitlab.com/simonpunk/susfs4ksu.git -b kernel-4.9 susfs4ksu
+cp susfs4ksu/kernel_patches/fs/* ./fs
+cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
+cp susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.9.patch ./
+patch -p1 < 50_add_susfs_in_kernel-4.9.patch
+make mrproper
 echo "# CONFIG_KPM is not set" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_KALLSYMS=y" >> ./arch/arm64/configs/mido_defconfig
 echo "CONFIG_KALLSYMS_ALL=y" >> ./arch/arm64/configs/mido_defconfig
