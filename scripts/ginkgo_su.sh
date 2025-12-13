a32227996fd91741b1e7268310250f995cc29b5c
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-12-13 15:56:24 +0700

    Update ginkgo_su.sh

--- scripts/ginkgo_su.sh
+++ scripts/ginkgo_su.sh
@@ -5,7 +5,6 @@ push
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/KOWX712/KernelSU/master/kernel/setup.sh" | bash -s master
 LOCAL_DIR="$(pwd)/.."
 TC_DIR="${LOCAL_DIR}/toolchain"
 CLANG_DIR="${TC_DIR}/clang"
