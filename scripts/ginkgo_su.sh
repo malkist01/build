5cef8f7a33f31922b260928e90124e2bbf0253c4
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-12-27 22:52:17 +0700

    Add files via upload

--- scripts/ginkgo_su.sh
+++ scripts/ginkgo_su.sh
@@ -92,14 +92,13 @@ tgs() {
 # Send Build Info
 sendinfo() {
     tg "
-• IMcompiler Action •
-*Building on*: \`Github actions\`
-*Date*: \`${DATE}\`
-*Device*: \`${DEVICE} (${CODENAME})\`
-*Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
-*Compiler*: \`${KBUILD_COMPILER_STRING}\`
-*Last Commit*: \`${COMMIT_HASH}\`
-*Build Status*: \`${STATUS}\`"
+• 🕊️Teletubiescompiler Action •
+* 💻 Building on*: \`Github actions\`
+* 📆 Date*: \`${DATE}\`
+* 📱Device*: \`${DEVICE} (${CODENAME})\`
+* 💼 Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
+* 🔗 Last Commit*: \`${COMMIT_HASH}\`
+* 🔨 Build Status*: \`${STATUS}\`"
 }
 
 # Push kernel to channel
@@ -132,13 +131,17 @@ compile() {
     make -j"${PROCS}" O=out \
        ARCH="arm64" \
        CC="clang" \
-       LD="ld.lld" \
-       AR="llvm-ar" \
-       AS="llvm-as" \
-       NM="llvm-nm" \
-       OBJCOPY="llvm-objcopy" \
+       READELF="llvm-readelf" \
+       OBJSIZE="llvm-size" \
        OBJDUMP="llvm-objdump" \
+       OBJCOPY="llvm-objcopy" \
        STRIP="llvm-strip" \
+       NM="llvm-nm" \
+       AR="llvm-ar" \
+       HOSTAR="llvm-ar" \
+       HOSTAS="llvm-as" \
+       HOSTNM="llvm-nm" \
+       LD="ld.lld" \
        CLANG_TRIPLE="aarch64-linux-gnu-" \
        CROSS_COMPILE="$ARCH_DIR/bin/aarch64-elf-" \
        CROSS_COMPILE_ARM32="$ARM_DIR/bin/arm-arm-eabi-" \
