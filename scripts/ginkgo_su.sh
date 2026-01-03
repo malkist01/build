40d94ff19961f083f5bbfca4396a2498d5209ab9
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-01-03 13:57:56 +0700

    Update ginkgo_su.sh

--- scripts/ginkgo_su.sh
+++ scripts/ginkgo_su.sh
@@ -5,7 +5,6 @@ push
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/FirmanBell/KernelSU-Next/legacy-test/kernel/setup.sh" | bash -s legacy-test
 LOCAL_DIR="$(pwd)/.."
 TC_DIR="${LOCAL_DIR}/toolchain"
 CLANG_DIR="${TC_DIR}/clang"
