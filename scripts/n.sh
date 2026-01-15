2669c744b3848f8f77b2ae0ae9f7b74c9ef830f9
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-01-15 16:18:26 +0700

    Update n.sh

--- scripts/n.sh
+++ scripts/n.sh
@@ -7,7 +7,7 @@ curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/ke
 patch -p1 < WildJames-Manager.patch
 patch -p1 < SUSFS-2.0.0.patch
 patch -p1 < seccomp.patch scope-min-hooks-v1.6.patch
-patch -p1 < susfs-2.0.0.patch
+patch -p1 -F 3 < susfs-2.0.0.patch
 sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/vendor/configs/ginkgo_defconfig
 LOCAL_DIR="$(pwd)/.."
 TC_DIR="${LOCAL_DIR}/toolchain"
