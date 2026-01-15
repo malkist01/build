1844ecbca8e615717fc893eea7347e3cbcdad15d
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-01-15 18:53:59 +0700

    Update ginkgo_su.sh

--- scripts/ginkgo_su.sh
+++ scripts/ginkgo_su.sh
@@ -3,7 +3,7 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
-apply patch -p1 -F 3 < susfs-1.5.12.patch
+patch -p1 -F 3 < susfs-1.5.12.patch
 LOCAL_DIR="$(pwd)/.."
 TC_DIR="${LOCAL_DIR}/toolchain"
 CLANG_DIR="${TC_DIR}/clang"
